7bfc59ba22d1b7f7ac58a936ad6048e596eab120
shricharan <rshricharan29@gmail.com>
2024-12-31 22:26:31 +0530

    add morgan and winston

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import express from "express";
 import "dotenv/config";
 
+import logger from "./logger";
+import morgan from "morgan";
+
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 const teas = [];
 let nextId = 1;
